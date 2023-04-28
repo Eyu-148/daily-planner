8de0e1dafe8649af6b49c004a4e8f4b3dbec0ab6
Chen Eyu <ychen49@uvm.edu>
2023-04-28 22:50:30 +0800

    index.js css rules, fonts

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 // @ts-check
 import React from "react";
 import '../styles/global.css'
+import '../styles/fonts.css'
 
 export default function App({Component, pageProps}) {
     return <Component {...pageProps} />;
