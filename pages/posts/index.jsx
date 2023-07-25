5f2f416fe187b65bda77adfe2af7df6adbee2e19
Chen Eyu <ychen49@uvm.edu>
2023-07-25 23:40:53 +0800

    update

--- pages/posts/index.jsx
+++ pages/posts/index.jsx
@@ -5,25 +5,11 @@ import PostPage from "./postPage";
 import Head from "next/head";
 import { BUDGET_POSTS } from "../../public/data/text_data";
 
-export async function getStaticProps() {
-    const allPostsData = BUDGET_POSTS
-    return {
-      props: {
-        allPostsData,
-      },
-    };
-  }
-
 export default function Posts({allPostsData}) {
     
     return (
         <Layout isHome={true}>
-            <Head>
-                <title>Posts</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
-            <PostPage isDelete={false} postsData={allPostsData} />
+            <p>post page</p>
         </Layout>
     )
 }
